de525d1723c45f4805bdf69b13266f0cdc252f52
Adriano Neves Rodrigues <addnrodrigues@gmail.com>
2022-08-08 11:25:32 -0300

    config: get api configs from the .env file

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: process.env.REACT_APP_LOCAL_API_URL,
     headers: {
-        Autorization: "A1LBSuqqU8C8hPHB-vynzZyLI5iwPix1v2mKNLnJlW6brL4NUdEweMOI3tuqajQXydTf7c_VUhuT3tNZETi28LbTE5qkvJHntsZEffWoJS9oDzRv7faLETrAie8fXiOsD4utBGKwh4z3UQwuRmAZvGYJcjRtJ7ytOriQLzegFZatGGpx0wTwAvufNqYeAOhYDTJLnUVqad5mbIFFUlrU_2LBXG_0z2rQxrv4fuHIar68HMmMoB-w-gVXCZ-O:feedlydev"
+        Autorization: process.env.REACT_APP_LOCAL_API_AUTHORIZATION
     }
 })
 
