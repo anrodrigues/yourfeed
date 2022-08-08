84700d63a4337850f4ef4f6110ffbf037aaaa178
Adriano Neves Rodrigues <addnrodrigues@gmail.com>
2022-08-08 11:23:33 -0300

    config: get .env variables

--- api/src/axios.js
+++ api/src/axios.js
@@ -1,12 +1,12 @@
 const  axios = require('axios');
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
-    
     get: axios.create({
-        baseURL: 'https://cloud.feedly.com/v3',
+        baseURL: process.env.FEEDLY_API_URL,
         headers: {
-            Autorization: "A1LBSuqqU8C8hPHB-vynzZyLI5iwPix1v2mKNLnJlW6brL4NUdEweMOI3tuqajQXydTf7c_VUhuT3tNZETi28LbTE5qkvJHntsZEffWoJS9oDzRv7faLETrAie8fXiOsD4utBGKwh4z3UQwuRmAZvGYJcjRtJ7ytOriQLzegFZatGGpx0wTwAvufNqYeAOhYDTJLnUVqad5mbIFFUlrU_2LBXG_0z2rQxrv4fuHIar68HMmMoB-w-gVXCZ-O:feedlydev"
+            Autorization:  process.env.FEEDLY_API_KEY
         }
     })
 
